3d3270f4cba9587c935dd95400db58a46e032110
Dmitry Safronov <dantewai523@gmail.com>
2021-11-06 11:24:53 +0300

    create base routing

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,26 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PasteController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+
+Route::name('pastes.')->prefix('pastes')->group(function(){
+    Route::get('/', [PasteController::class, 'index'])->name('index');
+    Route::get('/{id}', [PasteController::class, 'show'])->name('show');
+    Route::post('/', [PasteController::class, 'index'])->name('store');
+});
+
+Route::name('users.')->prefix('users')->group(function(){
+    Route::get('/', [UserController::class, 'index'])->name('index');
+    Route::post('/{id}', [UserController::class, 'show'])->name('show');
+});
+
+Route::name('auth.')->prefix('auth')->group(function(){
+    Route::post('/login', [AuthController::class, 'index'])->name('login');
+    Route::post('/register', [AuthController::class, 'index'])->name('register');
+    Route::post('/logout', [AuthController::class, 'index'])->name('logout');
 });
